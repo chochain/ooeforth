47c9ce89e204505f225aa68107864f9964f9df8c
Chochain Lee <chochain@yahoo.com>
2025-08-17 17:29:15 +0800

    ~/src/eforth/FV.java cleanup method naming

--- src/eforth/FV.java
+++ src/eforth/FV.java
@@ -6,13 +6,12 @@ package eforth;
 
 import java.util.ArrayList;
 
-final public class List<T> extends ArrayList<T> {
-    T head()           { return get(0);                 }
-    T tail()           { return get(size() - 1);        }
-    T tail(int offset) { return get(size() - offset);   }
+public class FV<T> extends ArrayList<T> {      ///< eForth ArrayList (i.e. vector)
+    T head()               { return get(0);                 }
+    T tail()               { return get(size() - 1);        }
+    T tail(int offset)     { return get(size() - offset);   }
     
-    List<T> set_head(T w) { set(0, w);        return this; }
-    List<T> drop_head()   { remove(0);        return this; }
-    List<T> drop_tail()   { remove(size()-1); return this; }
+    FV<T> merge(FV<T> lst) { addAll(lst);      return this; }
+    FV<T> drop()           { remove(size()-1); return this; }
 }
     

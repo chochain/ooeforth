bab4e7fe293681bf839a8aceb161c2c1481c15c4
Chochain Lee <chochain@yahoo.com>
2025-08-17 05:35:54 -0700

    ~/src/eforth/IO.java drop public prefix (in package public)

--- src/eforth/IO.java
+++ src/eforth/IO.java
@@ -10,7 +10,7 @@ import java.io.*;
 ///> console input/output
 ///
 public class IO {
-    public enum OP { CR, BL, EMIT, DOT, UDOT, DOTR, UDOTR, SPCS }
+    enum OP { CR, BL, EMIT, DOT, UDOT, DOTR, UDOTR, SPCS }
         
     Scanner         in   = null;
     StringTokenizer tok  = null;
@@ -26,26 +26,26 @@ public class IO {
         tok = nx ? new StringTokenizer(tib) : null;   ///< build tokenizer
         return nx;
     }
-    public String next_token() {
+    public void pstr(String s)   { out.print(s); }
+    public void err(Exception e) { e.printStackTrace(); }
+    
+    String next_token() {
         return tok.hasMoreTokens() ? tok.nextToken().trim() : null;
     }
-    public String scan(String delim) {
+    String scan(String delim) {
         String s = tok.nextToken(delim);              ///< read to delimiter
         tok.nextToken();                              ///< skip a blank
         return s;
     }
-
-    public void pstr(String s)   { out.print(s); }
-    public void err(Exception e) { e.printStackTrace(); }
     ///
     ///> IO methods
     ///
-    public int    key() { return (int)tok.nextToken().charAt(0); }
-    public String itoa(int n, int base) { return Integer.toString(n, base); }
-    public void spaces(int n) {
+    int    key() { return (int)tok.nextToken().charAt(0); }
+    String itoa(int n, int base) { return Integer.toString(n, base); }
+    void spaces(int n) {
         for (int i=0; i < Math.max(1,n); i++) out.print(" ");
     }
-    public void dot(OP op, int n, int r, int base) {
+    void dot(OP op, int n, int r, int base) {
         switch (op) {
         case CR:   out.println();                            break;
         case BL:   out.print(Character.toChars(0x20));       break;
@@ -63,18 +63,18 @@ public class IO {
         } break;
         }
     }
-    public void dot(OP op, int n, int r) { dot(op, n, r, 10); }
-    public void dot(OP op, int n)        { dot(op, n, 0, 10); }
-    public void cr()                     { dot(OP.CR, 0, 0, 10); }
-    public void bl()                     { dot(OP.BL, 0, 0, 10); }
+    void dot(OP op, int n, int r) { dot(op, n, r, 10); }
+    void dot(OP op, int n)        { dot(op, n, 0, 10); }
+    void cr()                     { dot(OP.CR, 0, 0, 10); }
+    void bl()                     { dot(OP.BL, 0, 0, 10); }
     ///
     ///> ok - stack dump and OK prompt
     ///
-    public void ss_dump(Stack<Integer> ss, int base) {
+    void ss_dump(Stack<Integer> ss, int base) {
         cr();
         for (int n : ss) out.print(Integer.toString(n, base)+" ");
     }
-    public void words(Dict dict) {
+    void words(Dict dict) {
         int i=0, sz = 0; 
         for (var w : dict) {
             pstr(w.name + "  ");
@@ -82,7 +82,7 @@ public class IO {
             if (sz > 64) { cr(); sz = 0; }
         }
     }
-    public void see(Code w) {
+    void see(Code w) {
         if (w == null) return;
         
         pstr(w.name+", "+w.token+", "+w.qf.toString());

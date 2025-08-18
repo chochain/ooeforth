374d2681318195b57bf3994a7f8da378e2b56dc0
Chochain Lee <chochain@yahoo.com>
2025-08-18 15:26:44 +0800

    ~/src/eforth/Dict.java tune find tracer

--- src/eforth/Dict.java
+++ src/eforth/Dict.java
@@ -25,9 +25,15 @@ final public class Dict extends FV<Code> {
     ///    @return      Code found; null - if not found
     ///
     Code find(String n, boolean compile)  {
+        System.out.printf("find %s", n);
         for (int i=dict.size()-(compile ? 2 : 1); i>=0; i--) { // search array from tail to head
-            Code w = dict.get(i); if (w.name == n) return w;
+            Code w = dict.get(i); 
+            if (n.equals(w.name)) {
+                System.out.printf(" => [%d]%s\n", w.token, w.name);
+                return w;
+            }
         }
+        System.out.printf(" => not found\n");
         return null;
     }
     Code compile(Code w) { dict.tail().pf.add(w); return w; }

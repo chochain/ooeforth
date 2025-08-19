19fc6d4e5edf96bb2dad7663e67245f03059b380
Chochain Lee <chochain@yahoo.com>
2025-08-19 16:59:28 +0800

    ~/src/eforth/IO.java clean up readline(); add load

--- src/eforth/IO.java
+++ src/eforth/IO.java
@@ -7,11 +7,14 @@ package eforth;
 import java.io.*;
 import java.util.*;
 import java.util.function.*;
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+//import java.io.IOException;
 ///
 ///> console input/output
 ///
 public class IO {
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
     enum OP { CR, BL, EMIT, DOT, UDOT, DOTR, UDOTR, SPCS }
 
     String      name;
@@ -35,11 +38,9 @@ public class IO {
         out.printf("\n%s, RAM %3.1f%% free (%d / %d MB)\n", name, pct, free, max);
     }
     public boolean readline() {
-        boolean t   = in.hasNextLine();                ///< any more line to read?
-        String  tib = t ? in.nextLine() : null;        ///< feed input line
-        debug("more="+t+" tib="+tib+"\n");
-        tok = t ? new Scanner(tib) : null;             ///< create the tokenizer
-        return t;
+        tok = in.hasNextLine()                         ///< create tokenizer
+            ? new Scanner(in.nextLine()) : null;   
+        return tok != null;
     }
     public void pstr(String s)   { out.print(s); out.flush(); }
     public void pchr(int n)      { out.print(Character.toChars(n)); }
@@ -91,7 +92,7 @@ public class IO {
     ///> ok - stack dump and OK prompt
     ///
     void ss_dump(Stack<Integer> ss, int base) {
-        cr(); for (int n : ss) pstr(itoa(n, base)+" ");
+        for (int n : ss) pstr(itoa(n, base)+" ");
     }
     void words(Dict dict) {
         int i=0, sz = 0; 
@@ -100,6 +101,7 @@ public class IO {
             sz += w.name.length() + 2;                         /// width control
             if (sz > 64) { cr(); sz = 0; }
         }
+        cr();
     }
     void see(Code c, int dp) {
         if (c==null) return;
@@ -121,5 +123,20 @@ public class IO {
         }
         if (c.str != null)  pstr(" \\ =\""+c.str+"\" ");
         if (dp == 0) pstr("\n;");
+    }
+    void load(VM vm, String fn) {
+        debug("loading "+fn+"...\n");
+        Scanner in0 = in;
+        int i = 0;
+        try (Scanner sc = new Scanner(new File(fn))) {
+            in = sc;
+            while (readline()) {
+                i++;
+                if (!vm.outer()) break;
+            }
+        }
+        catch (IOException e) { err(e); }
+        debug(fn + " loaded "+i+" lines\n");
+        in = in0;
     }
 }

36c9218360fb5c7a09a316ac09879089e5538b8a
Chochain Lee <chochain@yahoo.com>
2025-08-19 16:58:43 +0800

    ~/src/Eforth.java prompt io.mstat=>vm.ok; add comments

--- src/Eforth.java
+++ src/Eforth.java
@@ -11,16 +11,16 @@ public class Eforth implements Runnable {                   /// ooeforth
     VM           vm;                                        ///< eForth virtual machine
 
     public Eforth(InputStream in, PrintStream out) {
-        io = new IO(APP_NAME, in, out);
-        vm = new VM(io);
+        io = new IO(APP_NAME, in, out);                     ///< instantiate IO
+        vm = new VM(io);                                    ///< instantiate VM
     }
     
     public void run() {
-        io.mstat();
-        while (io.readline()) {
-            if (!vm.outer()) break;
+        vm.ok(true);                                        /// * prompt VM ready
+        while (io.readline()) {                             /// * fetch from input 
+            if (!vm.outer()) break;                         /// * outer interpreter
         }
-        io.pstr("\n"+APP_NAME+" Done.\n");
+        io.pstr("\n"+APP_NAME+" Done.\n");                  /// * exit prompt
     }
 
     public static void main(String args[]) {                /// main app
